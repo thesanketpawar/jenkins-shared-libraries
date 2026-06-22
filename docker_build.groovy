a79ee287ba9058dad7f9d3bcdff948358628c8fb
Sanket Pawar <125241311+thesanketpawar@users.noreply.github.com>
2026-06-22 15:07:02 +0530

    Update docker_build.groovy

--- docker_build.groovy
+++ docker_build.groovy
@@ -1,3 +1,4 @@
-def call (String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t $dockerHubUser/${ProjectName}:${ImageTag} ."
+def call(String projectName, String imageTag, String dockerHubUser) {
+    echo "🐳 Shared Library: Building Image -> ${dockerHubUser}/${projectName}:${imageTag}"
+    sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
