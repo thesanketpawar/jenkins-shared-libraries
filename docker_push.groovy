292eadbcceccb57621675c9d36529e92f0949738
Sanket Pawar <125241311+thesanketpawar@users.noreply.github.com>
2026-06-22 15:07:24 +0530

    Update docker_push.groovy

--- docker_push.groovy
+++ docker_push.groovy
@@ -1,6 +1,17 @@
-def call (String ProjectName, String ImageTag , String dockerHubUser) {
-  withCredentials([usernamePassword('credentialsId':'dockerHubCred', passwordVariable:'dockerHubPass', usernameVariable:'dockerHubUser')]) {
-  sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-  }
-sh "sh "docker push ${env.dockerHubUser}/${ProjectName}:${ImageTag}"
+def call(String projectName, String imageTag, String dockerHubUser) {
+    echo "🚀 Shared Library: Pushing Image to Docker Hub..."
+    
+    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', 
+                                      passwordVariable: 'dockerHubPass', 
+                                      usernameVariable: 'dockerHubUser')]) {
+        
+        // 1. Docker Hub mein Login karo
+        sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+        
+        // 2. Image ko correct user account ke sath tag karo
+        sh "docker image tag notes:${imageTag} ${env.dockerHubUser}/${projectName}:${imageTag}"
+        
+        // 3. Image ko push karo (withCredentials ke andar hi hona chahiye)
+        sh "docker push ${env.dockerHubUser}/${projectName}:${imageTag}"
+    }
 }
